a8d63fdfeeb074285f46abe93c40aea8a733a2a4
weituotian <921977939@qq.com>
2017-02-20 19:01:38 +0800

    修复全屏按钮不能退出全屏的问题

--- dist/js/app_iframe.js
+++ dist/js/app_iframe.js
@@ -98,6 +98,8 @@ var App = function () {
         });
     };
 
+    var isFullScreen = false;
+
     var requestFullScreen = function () {
         var de = document.documentElement;
 
@@ -113,6 +115,42 @@ var App = function () {
         }
 
     };
+
+    var requestFullScreen2 = function (element) {
+        // 判断各种浏览器，找到正确的方法
+        var requestMethod = element.requestFullScreen || //W3C
+            element.webkitRequestFullScreen ||    //Chrome等
+            element.mozRequestFullScreen || //FireFox
+            element.msRequestFullScreen; //IE11
+        if (requestMethod) {
+            requestMethod.call(element);
+        }
+        else if (typeof window.ActiveXObject !== "undefined") {//for Internet Explorer
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
+        }
+    };
+
+    //退出全屏 判断浏览器种类
+    var exitFull = function () {
+        // 判断各种浏览器，找到正确的方法
+        var exitMethod = document.exitFullscreen || //W3C
+            document.mozCancelFullScreen ||    //Chrome等
+            document.webkitExitFullscreen || //FireFox
+            document.webkitExitFullscreen; //IE11
+        if (exitMethod) {
+            exitMethod.call(document);
+        }
+        else if (typeof window.ActiveXObject !== "undefined") {//for Internet Explorer
+            var wscript = new ActiveXObject("WScript.Shell");
+            if (wscript !== null) {
+                wscript.SendKeys("{F11}");
+            }
+        }
+    };
+
     // handle the layout reinitialization on window resize
     var handleOnResize = function () {
         var resize;
@@ -702,7 +740,13 @@ var App = function () {
             handleBootstrapConfirmation(); // handle bootstrap confirmations
         },
         handleFullScreen: function () {
-            requestFullScreen();
+            if (isFullScreen) {
+                exitFull();
+                isFullScreen = false;
+            } else {
+                requestFullScreen();
+                isFullScreen = true;
+            }
         },
         fixIframeTab: function () {
             handleIframeContent();
